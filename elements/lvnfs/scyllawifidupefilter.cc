a8e6fd67507de54a7bffa241cba15acb59d509b7
Roberto Riggio <roberto.riggio@gmail.com>
2015-08-07 14:57:36 +0200

    keep track of the last N sequence numbers in a circular buffer

--- elements/lvnfs/scyllawifidupefilter.cc
+++ elements/lvnfs/scyllawifidupefilter.cc
@@ -11,14 +11,16 @@
 CLICK_DECLS
 
 ScyllaWifiDupeFilter::ScyllaWifiDupeFilter() :
-		_debug(false) {
+		_debug(false), _buffer_size(10) {
 }
 
 ScyllaWifiDupeFilter::~ScyllaWifiDupeFilter() {
 }
 
 int ScyllaWifiDupeFilter::configure(Vector<String> &conf, ErrorHandler *errh) {
-	return Args(conf, this, errh).read("DEBUG", _debug).complete();
+	return Args(conf, this, errh).read("BUFFER_SIZE", _buffer_size)
+			                     .read("DEBUG", _debug)
+						         .complete();
 }
 
 Packet *
@@ -34,30 +36,28 @@ ScyllaWifiDupeFilter::simple_action(Packet *p_in) {
 	EtherAddress dst = EtherAddress(w->i_addr1);
 	uint16_t seq = le16_to_cpu(w->i_seq) >> WIFI_SEQ_SEQ_SHIFT;
 	uint8_t frag = le16_to_cpu(w->i_seq) & WIFI_SEQ_FRAG_MASK;
-	u_int8_t more_frag = w->i_fc[1] & WIFI_FC1_MORE_FRAG;
+	uint8_t more_frag = w->i_fc[1] & WIFI_FC1_MORE_FRAG;
 
 	bool is_frag = frag || more_frag;
 
 	ScyllaDupeFilterDstInfo *nfo = _table.findp(src);
 
-	if ((w->i_fc[0] & WIFI_FC0_TYPE_CTL) || dst.is_group()) {
+	if ((w->i_fc[0] & WIFI_FC0_TYPE_CTL) || dst.is_group() || is_frag) {
 		return p_in;
 	}
 
 	if (!nfo) {
-		_table.insert(src, ScyllaDupeFilterDstInfo(src));
+		_table.insert(src, ScyllaDupeFilterDstInfo(src, _buffer_size));
 		nfo = _table.findp(src);
 	}
 
-	if (seq == nfo->_seq && (!is_frag || frag <= nfo->_frag)) {
+	if (nfo->_buffer->contains(seq)) {
 		nfo->_dupes++;
 		p_in->kill();
 		return 0;
 	}
 
-	nfo->_seq = seq;
-	nfo->_frag = frag;
-
+	nfo->_buffer->add(seq);
 	return p_in;
 }
 
@@ -74,9 +74,7 @@ String ScyllaWifiDupeFilter::read_handler(Element *e, void *thunk) {
 	case H_STATE_TABLE: {
 		StringAccum sa;
 		for (DupeIter it = td->table()->begin(); it.live(); it++) {
-			sa << it.value()._eth.unparse() << ' ' << it.value()._dupes << ' '
-					<< it.value()._packets << ' ' << it.value()._seq << ' '
-					<< it.value()._frag << "\n";
+			sa << it.value()._eth.unparse() << ' ' << it.value()._dupes << ' ' << it.value()._buffer_size << it.value()._buffer->unparse() << ' '  << "\n";
 		}
 		return sa.take_string();
 	}
@@ -100,29 +98,30 @@ int ScyllaWifiDupeFilter::write_handler(const String &in_s, Element *e,
 	case H_STATE_TABLE: {
 		Vector<String> tokens;
 		cp_spacevec(s, tokens);
-		if (tokens.size() != 5)
-			return errh->error("state_table accepts 5 parameters");
+		if (tokens.size() < 4)
+			return errh->error("state_table requires at least 4 parameters <eth> <dupes> <N> <seq1> ... <seqN>");
 		EtherAddress eth;
 		int dupes;
-		int packets;
-		uint16_t seq;
-		uint16_t frag;
+		int buffer_size;
 		if (!EtherAddressArg().parse(tokens[0], eth)) {
 			return errh->error("error param %s: must start with Ethernet address", tokens[0].c_str());
 		}
 		if (!IntArg().parse(tokens[1], dupes)) {
 			return errh->error("error param %s: must start with int", tokens[1].c_str());
 		}
-		if (!IntArg().parse(tokens[1], packets)) {
+		if (!IntArg().parse(tokens[2], buffer_size)) {
 			return errh->error("error param %s: must start with int", tokens[2].c_str());
 		}
-		if (!IntArg().parse(tokens[1], seq)) {
-			return errh->error("error param %s: must start with int", tokens[3].c_str());
+		SeqBuffer * buffer = new SeqBuffer(buffer_size);
+		for (int i = 3; i < tokens.size(); i++) {
+			int seq;
+			if (!IntArg().parse(tokens[i], seq)) {
+				return errh->error("error param %s: must start with int", tokens[i].c_str());
+			}
+			click_chatter("adding %u", seq);
+			buffer->add(seq);
 		}
-		if (!IntArg().parse(tokens[1], frag)) {
-			return errh->error("error param %s: must start with int", tokens[4].c_str());
-		}
-		f->table()->insert(eth, ScyllaDupeFilterDstInfo(eth, dupes, packets, seq, frag));
+		f->table()->insert(eth, ScyllaDupeFilterDstInfo(eth, dupes, buffer_size, buffer));
 		break;
 	}
 	}
--- elements/lvnfs/scyllawifidupefilter.hh
+++ elements/lvnfs/scyllawifidupefilter.hh
@@ -5,29 +5,113 @@
 #include <click/hashmap.hh>
 CLICK_DECLS
 
+class SeqBuffer {
+public:
+	SeqBuffer(unsigned int period) :
+		period(period), window(new int[period]), head(NULL), tail(NULL) {
+		assert(period >= 1);
+	}
+	~SeqBuffer() {
+		delete[] window;
+	}
+	// Adds a value to the average, pushing one out if necessary
+	void add(int val) {
+		// Special case: Initialisation
+		if (head == NULL) {
+			head = window;
+			*head = val;
+			tail = head;
+			inc(tail);
+			return;
+		}
+		// Were we already full?
+		if (head == tail) {
+			// Make room
+			inc(head);
+		}
+		// Write the value in the next spot.
+		*tail = val;
+		inc(tail);
+	}
+
+	String unparse() {
+		StringAccum sa;
+		if (!head) {
+			return sa.take_string();
+		}
+		int * ptr = head;
+		do {
+			sa << ' ' << *ptr;
+			inc(ptr);
+		} while (ptr != head);
+		return sa.take_string();
+	}
+
+	bool contains(int val) {
+		if (!head) {
+			return false;
+		}
+		int * ptr = head;
+		do {
+			if (val == *ptr) {
+				return true;
+			}
+			inc(ptr);
+		} while (ptr != head);
+		return false;
+	}
+
+private:
+
+	unsigned int period;
+	int * window; // Holds the sequence numbers
+
+	// Logically, head is before tail
+	int * head; // Points at the oldest element we've stored.
+	int * tail; // Points at the newest element we've stored.
+
+	// Bumps the given pointer up by one.
+	// Wraps to the start of the array if needed.
+	void inc(int * & p) {
+		if (++p >= window + period) {
+			p = window;
+		}
+	}
+
+	// Returns how many sequence numbers
+	ptrdiff_t size() const {
+		if (head == NULL)
+			return 0;
+		if (head == tail)
+			return period;
+		return (period + tail - head) % period;
+	}
+
+};
+
 class ScyllaDupeFilterDstInfo {
 public:
 	EtherAddress _eth;
 	int _dupes;
-	int _packets;
-	uint16_t _seq;
-	uint16_t _frag;
-	ScyllaDupeFilterDstInfo() :
-			_dupes(0), _packets(0), _seq(0), _frag(0) {
+	int _buffer_size;
+	SeqBuffer * _buffer;
+	ScyllaDupeFilterDstInfo() {
+		_eth = EtherAddress();
+		_dupes = 0;
+		_buffer_size = 5;
+		_buffer = new SeqBuffer(_buffer_size);
 	}
-	ScyllaDupeFilterDstInfo(EtherAddress eth) :
-			_dupes(0), _packets(0), _seq(0), _frag(0) {
+	ScyllaDupeFilterDstInfo(EtherAddress eth, int buffer_size) {
 		_eth = eth;
+		_dupes = 0;
+		_buffer_size = buffer_size;
+		_buffer = new SeqBuffer(_buffer_size);
 	}
-	ScyllaDupeFilterDstInfo(EtherAddress eth, int dupes, int packets,
-			uint16_t seq, uint16_t frag) {
+	ScyllaDupeFilterDstInfo(EtherAddress eth, int dupes, int buffer_size, SeqBuffer * buffer) {
 		_eth = eth;
 		_dupes = dupes;
-		_packets = packets;
-		_seq = seq;
-		_frag = frag;
-	}
-	void clear() {
+		_buffer_size = buffer_size;
+		_buffer = buffer;
 	}
 };
 
@@ -57,6 +141,7 @@ class ScyllaWifiDupeFilter : public Element {
 private:
 
   bool _debug;
+  int _buffer_size;
 
   DupeTable _table;
 
