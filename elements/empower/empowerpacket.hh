4cf925509b0599dc95bc5dab4ea64b38e7b0de2b
Roberto Riggio <roberto.riggio@gmail.com>
2016-07-09 17:25:00 +0200

    add flags to lvap stats

--- elements/empower/empowerpacket.hh
+++ elements/empower/empowerpacket.hh
@@ -222,11 +222,14 @@ public:
 /* lvap_stats entry format */
 struct lvap_stats_entry {
   private:
-    uint8_t _rate; 	/* Rate in units of 500kbps or MCS index */
-    uint32_t _prob; /* Probability [0-18000] */
+    uint8_t 	_rate; 	/* Rate in units of 500kbps or MCS index */
+    uint16_t _flags;	/* Flags (empower_rate_flags) */
+    uint32_t _prob; 	/* Probability [0-18000] */
   public:
-    void set_rate(uint8_t rate)  { _rate = rate; }
-    void set_prob(uint32_t prob) { _prob = htonl(prob); }
+    void set_flags(uint16_t flags)    	{ _flags = htons(flags); }
+    void set_flag(uint16_t f)        	{ _flags = htons(ntohs(_flags) | f); }
+    void set_rate(uint8_t rate)  		{ _rate = rate; }
+    void set_prob(uint32_t prob) 		{ _prob = htonl(prob); }
 } CLICK_SIZE_PACKED_ATTRIBUTE;
 
 /* channel quality map request packet format */
